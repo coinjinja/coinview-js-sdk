5882b9487956c65630c9c0b9289277a7cf4641ec
Hsiaoming Yang <me@lepture.com>
2018-08-28 19:08:31 +0900

    Build dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,25 @@
 import babel from 'rollup-plugin-babel'
 
-export default {
-  input: 'src/index.js',
-  plugins: [
-    babel({exclude: 'node_modules/**'})
-  ],
-  output: {
-    file: 'dist/coinview.js',
-    name: 'coinview',
-    format: 'iife'
+function getConfig (target) {
+  const config = {
+    input: 'src/index.js',
+    plugins: [
+      babel({exclude: 'node_modules/**'})
+    ]
   }
+  if (target === 'common') {
+    config.output = {
+      file: 'dist/coinview.common.js',
+      format: 'cjs'
+    }
+  } else if (target === 'window') {
+    config.output = {
+      file: 'dist/coinview.js',
+      name: 'coinview',
+      format: 'iife'
+    }
+  }
+  return config
 }
+
+export default getConfig(process.env.TARGET)
