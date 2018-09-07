ecefb459f45b9fc5c80533eed538a15acab869f4
Jesse Zhu <jesse@itjesse.com>
2018-09-07 11:41:02 +0900

    Complete index.d.js

--- dist/methods.js
+++ dist/methods.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _default = ['user.profile', 'user.assets', 'user.address', 'payment.create', 'navigate.back', 'navigate.close', 'navigate.toggleNavBar', 'ui.notice', 'ui.confirm', 'utils.choosePhoto', 'utils.setClipboard', 'utils.share', 'utils.scanQr', 'utils.openUrl'];
+var _default = ['user.profile', 'user.assets', 'user.address', 'payment.create', 'navigate.back', 'navigate.close', 'navigate.toggleNavBar', 'ui.notice', 'ui.confirm', 'utils.setClipboard', 'utils.share', 'utils.scanQr', 'utils._choosePhoto', 'utils._openUrl'];
 exports.default = _default;
--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,19 @@ export default interface Coinview {
   init(config: Config): Promise<Coinview>
   user: {
     profile(): Promise<UserProfile>
+    assets(): Promise<ReadonlyArray<UserAsset>>
+    address(assetId: string): Promise<UserAddress>
+  }
+  utils: {
+    setClipboard(payload: string): Promise<void>
+    scanQr(): Promise<string | null>
+  }
+  navigate: {
+    back(): Promise<void>
+    close(): Promise<void>
+  }
+  payment: {
+    create(payload: PaymentPayload): Promise<PaymentSnapshot>
   }
 }
 
@@ -21,4 +34,35 @@ interface Config {
 }
 
 interface UserProfile {
+  nickname: string
+  userId: string
+}
+
+interface UserAsset {
+  assetId: string
+  name: string
+  symbol: string
+  iconUrl: string
+  balance: string
+}
+
+interface UserAddress {
+  assetId: string
+  address: string
+}
+
+interface PaymentPayload {
+  traceId?: string
+  assetId: string
+  amount: number
+  memo?: string
+  desc: string
+}
+
+interface PaymentSnapshot {
+  snapshotId: string
+  traceId: string
+  assetId: string
+  mixinId: string
+  amount: number
 }
